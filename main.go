4d94c9bdce8c188b406f4b6147c7a5904ddd4a2e
Joe Wilson <wilsonmangunsong@gmail.com>
2023-04-18 21:25:39 +0700

    Deep dive with pointers and derefrence

--- main.go
+++ main.go
@@ -12,6 +12,15 @@ func zeroptr(iptr *int) {
 	// dan disini kita akan meng-assign value 0 ke variable pointer address tersebut
 }
 
+/*
+* Pointer Golang
+* Source: https://www.youtube.com/watch?v=a4HcEsJ1hIE&ab_channel=TechWithTim
+*
+* Pointer Golang
+* & (ampersand) -> get the pointer / mengambil address dr sebuah variable
+* * (asterix) -> derefrence
+ */
+
 func main() {
 	// 1 ====================================================== //
 	i := 1
@@ -77,4 +86,49 @@ func main() {
 
 	// End of 3 ===================================================== //
 
+	// 4 =========================================================== //
+	x := 7
+	//y := &x
+	var y *int // ini sama
+	y = &x     // dengan y := &x
+	// fmt.Println(*x) // tidak bisa print, cannot indirect
+	fmt.Println(&x) // value would be the variable address (the reference of x)
+	fmt.Println(y)  // would print the reference of x
+	fmt.Println(*y) // ini bisa, tidak seperti (*x) di atas, karena ini hanya bisa di lakukan oleh variable yg me-reference pointer dr variable lain
+
+	fmt.Println(x, y) // print before change the value of y which pointer to x. x = 7
+	*y = 8
+	fmt.Println(x, y) // x = 8
+	// End of 4 ================================================== //
+
+	// 5 =========================================================== //
+	toChange := "hello"
+	fmt.Println(toChange) // before
+	changeValue(&toChange)
+	fmt.Println(toChange) // after, value of toChange is changed to "changed!"
+
+	toChange2 := "hello2"
+	fmt.Println(toChange2) // before
+	cantChangeValue(toChange2)
+	fmt.Println(toChange2) // after, can't change the value because the param of cantChangeValue is not pointer
+
+	toChange3 := "hello3"
+	fmt.Println(toChange3) // before
+	toChange3 = changeValue2(toChange3)
+	fmt.Println(toChange3) // after, value change by copying a value from variable inside function
+	// End of 5 ==================================================== //
+
+}
+
+func changeValue(str *string) { // remember this? the param would be the variable that pointers to variable that has string type
+	*str = "changed!"
+}
+
+func cantChangeValue(str string) {
+	str = "can't changed!"
+}
+
+func changeValue2(str string) string {
+	str = "changed, but copying value not change the reference"
+	return str
 }
